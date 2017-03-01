c535e65529d3733ed45e44b2b15b09a21ff76775
Abigail White <abigailwhite213@gmail.com>
2017-03-01 09:42:33 -0800

    start wed

--- public/scripts/model/results.js
+++ public/scripts/model/results.js
@@ -1,9 +1,4 @@
 'use strict';
-// var getEvents = function(userZip)  {
-//   let urlZip = `meetup/2/open_events?zip=${userZip}&and_text=False&offset=0&format=json&limited_events=False&photo-host=public&page=20&radius=25.0&category=26&desc=False&status=upcoming`;
-//   $.get(urlZip)
-//   .then(data => console.log(data));
-// }
 
 (function(module){
   const results = {};
@@ -19,9 +14,6 @@
     .then(function() {
       results.locations = results.all.map(getLocation);
     })
-    // .then(callback());
-    // .then(data.results => results.locations = data.results.map(getLocation)
-    // .then(callback);
   };
 
   function getLocation(event) {
