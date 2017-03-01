2ee4d89fbe44259bfeb7b9a21c7d1e5ffabb01ed
Abigail White <abigailwhite213@gmail.com>
2017-03-01 12:02:36 -0800

    map rendered

--- public/scripts/view/resultsView.js
+++ public/scripts/view/resultsView.js
@@ -39,12 +39,6 @@
       google.maps.event.addListener(marker, 'click', function() {
         console.log('inside addlistener', marker);
       }
-      //   marker, 'click', function(marker) {
-      //   // return function() {
-      //   //   infoWindow.setContent(location[2]);
-      //   //   infoWindow.open(map,marker);
-      //   // };
-      // }
     );
 
       map.fitBounds(bounds);
@@ -55,25 +49,6 @@
       });
     });
 
-    // let codeFellows = {
-    //   lat:  47.618217,
-    //   lng: -122.3540207
-    // };
-    // let map = new google.maps.Map(document.getElementById('map'), {
-    //   zoom: 15,
-    //   center: codeFellows
-    // });
-    // let marker = new google.maps.Marker({
-    //   position: codeFellows,
-    //   map: map
-    // });
-    // let marker2 = new google.maps.Marker({
-    //   position: {
-    //     lat: 47.6210848,
-    //     lng: -122.352722
-    //   },
-    //   map: map
-    // });
   }
 
 module.resultsView = resultsView;
