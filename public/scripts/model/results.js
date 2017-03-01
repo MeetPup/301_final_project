fae2569a97a2a49bfb48ee3c17b4a3143f502d0c
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-28 17:42:41 -0800

    Store location and venue boolean in new array

--- public/scripts/model/results.js
+++ public/scripts/model/results.js
@@ -16,7 +16,9 @@
     let urlZip = `meetup/2/open_events?zip=${userZip}&and_text=False&offset=0&format=json&limited_events=False&photo-host=public&page=20&radius=25.0&category=26&desc=False&status=upcoming`;
     $.get(urlZip)
     .then(data => results.all = data.results, err => console.error(err))
-    // .then(results.locations = results.all.map(getLocation))
+    .then(function() {
+      results.locations = results.all.map(getLocation);
+    })
     // .then(callback());
     // .then(data.results => results.locations = data.results.map(getLocation)
     // .then(callback);
@@ -30,7 +32,7 @@
       return [lat, lon, venue];
     } else {
       let lat = event.group.group_lat;
-      let lon = group.group_lon;
+      let lon = event.group.group_lon;
       let venue = false;
       return [lat, lon, venue];
     }
